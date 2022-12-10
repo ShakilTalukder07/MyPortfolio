37bf23002726dc20c2003dfd2276953168ff640f
ShakilTalukder07 <shakil.talukder07@gmail.com>
2022-12-11 00:12:12 +0600

    favicon add

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -13,10 +13,11 @@ const Navbar = () => {
                 </div>
                 <div className="flex-none">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a href='/' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}>Home</a></li>
+                        <li><a href='/' onClick={() => setActiveNav('/')} className={activeNav === '/' ? 'active' : ''}>Home</a></li>
                         <li><a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'about' : ''}>About</a></li>
                         <li><a href='#experience'>Experience</a></li>
                         <li><a href="#services">Services</a></li>
+                        <li><a href="#testimonial">Testimonials</a></li>
                         <li><a href="#contact">Contact</a></li>
                     </ul>
                 </div>
