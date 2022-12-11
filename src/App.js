201776924e84875f3f17eae77e04a6aa1e6181e3
ShakilTalukder07 <shakil.talukder07@gmail.com>
2022-12-11 14:59:03 +0600

    router setu

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import './App.css';
-import About from './components/About/About';
-import Header from './components/Header/Header';
-import Navbar from './components/Navbar/Navbar';
-import Experience from './components/Experience/Experience'
-import Service from './components/Service/Service';
-// import Testimonials from './components/Testimonials/Testimonials';
-import Portfolio from './components/Portfolio/Portfolio';
-import Contact from './components/Contact/Contact';
-import Footer from './components/Footer/Footer';
+
+import { RouterProvider } from 'react-router-dom';
+import router from './Routes/Route';
 
 
 function App() {
   return (
     <>
-      <Navbar></Navbar>
-      <Header></Header>
-      <About></About>
-      <Experience></Experience>
-      <Service></Service>
-      <Portfolio></Portfolio>
-      {/* <Testimonials></Testimonials> */}
-      <Contact></Contact>
-      <Footer></Footer>
+    <RouterProvider router={router}>
+    </RouterProvider>
     </>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Header from '../Header/Header';
+import Portfolio from '../Portfolio/Portfolio';
+import Service from '../Service/Service';
+import Experience from '../Experience/Experience';
+import About from '../About/About';
+import Testimonials from '../Testimonials/Testimonials';
+import Contact from '../Contact/Contact';
+
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <About></About>
+            <Experience></Experience>
+            <Service></Service>
+            <Portfolio></Portfolio>
+            <Testimonials></Testimonials>
+            <Contact></Contact>
+        </div>
+    );
+};
+
+export default Home;
